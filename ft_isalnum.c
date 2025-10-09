51d0d11e4eaa713efc82852de8118c381bbefee5
l-moreau <lindsaymoreau2003@gmail.com>
2025-10-09 16:09:57 +0200

    mod digit & isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,26 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmoreau <lmoreau@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/09 15:51:06 by lmoreau           #+#    #+#             */
-/*   Updated: 2025/10/09 16:07:34 by lmoreau          ###   ########.fr       */
+/*   Created: 2025/10/09 15:58:45 by lmoreau           #+#    #+#             */
+/*   Updated: 2025/10/09 16:08:59 by lmoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isdigit(int c)
+int ft_isalnum(int c)
 {
-    if ((c >= '0' && c <= '9'))
-        return (c);
+    if ((c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' 
+            || c >= '0' && c <= '9'))
+            return (c);
     return (0);
 }
 /*#include <stdio.h>
 #include <ctype.h>
 int main()
 {
-	printf("%d\n", ft_isdigit('q'));
-	printf("%d\n", isdigit('q'));
+	printf("%d\n", ft_isalnum(','));
+    printf("%d\n", isalnum(','));
 	return 0;
 }*/
