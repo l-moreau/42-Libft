e9867bd7eed2d2ad58256ea51069886ab8429211
l-moreau <lindsaymoreau2003@gmail.com>
2025-10-21 12:17:55 +0200

    Added memmove and memcpy

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,36 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmoreau <lmoreau@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/20 13:24:38 by lmoreau           #+#    #+#             */
-/*   Updated: 2025/10/21 12:13:33 by lmoreau          ###   ########.fr       */
+/*   Created: 2025/10/21 11:09:53 by lmoreau           #+#    #+#             */
+/*   Updated: 2025/10/21 11:59:08 by lmoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char		*d;
 	const unsigned char	*s;
 
-	d = (unsigned char *)dest;
-	s = (const unsigned char *)src;
 	if (!dest && !src)
 		return (NULL);
-	while (n--)
-		*d++ = *s++;
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (d < s)
+	{
+		while (n--)
+			*d++ = *s++;
+	}
+	else if (d > s)
+	{
+		d += n;
+		s += n;
+		while (n--)
+			*(--d) = *(--s);
+	}
 	return (dest);
 }
+
 /*#include <stdio.h>
+#include <string.h>
 int main()
 {
-    char a[20] = "Bonjour";
-    char b[20];
-    ft_memcpy(b, a, 20);
-    printf("%s\n", b);
-    return 0;
+	char str[20] = "Hello, World!";
+	
+	ft_memmove(str + 7, str, 7);
+	printf("%s\n", str);
+
+	return 0;
 }*/
